0fe81f00629f7e97ebf0141561a16fb40790a89e
vntrp <grau.cyprien@gmail.com>
2015-09-14 21:48:12 +0000

    sort array of objects

--- functions.js
+++ functions.js
@@ -21,3 +21,32 @@ var NewArray = ["hola", "que", "balancoire", "casserole"];
 NewArray.sort(twoStringsSort)       // ["que", "hola", "casserole", "balancoire"]
 
 
+// (2) Create an array of objects (don’t need to use new here, just regular object literals).
+// These objects will contain a name and email property. Then, run some code that will sort your array by the longest name.
+// Then, run some code that will sort your array by e-mail address in alphabetical order.
+
+var customers = [
+    {name:"jack", email: "jack@gmail.com"},
+    {name: "bob", email: "bob@gmail.com"},
+    {name: "susan", email: "susan@gmail.com"},
+    {name: "emmanuel", email: "emmanuel@gmail.com"}
+]
+
+Array.prototype.sortBy = function(param) {
+  return this.slice(0).sort(function(strg1,strg2) {
+    return (strg1[param].length > strg2[param].length) ? 1 : (strg1[param].length < strg2[param].length) ? -1 : 0;
+  });
+}
+
+customers.sortBy('name');
+
+// part2
+
+Array.prototype.sortBy = function(param) {
+  return this.slice(0).sort(function(strg1,strg2) {
+    return (strg1[param] > strg2[param]) ? 1 : (strg1[param] < strg2[param]) ? -1 : 0;
+  });
+}
+
+customers.sortBy('email');
+
