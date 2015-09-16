bc30c77f5ba2d4d2ff7a6a253b838884bf5d7eb9
vntrp <grau.cyprien@gmail.com>
2015-09-16 23:23:31 +0000

    constructor 1.0

--- terrain.js
+++ terrain.js
@@ -1,2 +1,16 @@
 // usual comment again
 
+function Tile(x,y){
+    this.x=x;
+    this.y=y;
+    this.height=Math.ceil(Math.random()*3);
+    this.type=function(){
+        var tp=["grass", "rocks", "water"]
+        console.log(tp[Math.floor(Math.random()*3)]);
+    }
+    this.condition=function(){
+        var cdt=["burning", "normal", "frozen"]
+        console.log(cdt[Math.floor(Math.random()*3)]);
+    }
+    
+}
