2af2887c06fd5fc1cdb45bd6e820dc506d79ec92
vntrp <grau.cyprien@gmail.com>
2015-09-16 22:12:10 +0000

    fights!

--- warriors.js
+++ warriors.js
@@ -1,8 +1,8 @@
 // this is my usual line of comment
 
 function Warrior(name, gender){
-    this.name="name";
-    this.gender="gender";
+    this.name=name;
+    this.gender=gender;
     this.level=1;
     this.weapon="wooden sword";
     this.power=Math.round(Math.random()*100);
@@ -19,4 +19,19 @@ function Warrior(name, gender){
         }
     }
 }
+
+var warrior1=new Warrior("Barbara", "F");
+
+var warrior2=new Warrior("Stephania", "F");
+
+var warrior3=new Warrior("Wilfrid", "M");
+
+var warrior4=new Warrior("Wolfgang", "M");
+
+var warrior5=new Warrior("Amadeus", "M");
+
+warrior1.faceoff(warrior5);
+warrior3.faceoff(warrior4);
+warrior2.faceoff(warrior1);
+
 
